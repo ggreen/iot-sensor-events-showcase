8e997940f2bd5c68006fdddcdfd9fcc80c57299c
ggreen <gregoryg@vmware.com>
2025-11-28 21:08:51 -0500

    adding config

--- applications/iot-event-dashboard/build.gradle.kts
+++ applications/iot-event-dashboard/build.gradle.kts
@@ -30,26 +30,16 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
 	implementation("com.github.nyla-solutions:nyla.solutions.core:1.5.0")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 //	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-//	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.cloud:spring-cloud-stream")
 	implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
 	runtimeOnly("org.postgresql:postgresql")
-//	implementation("io.reactivex.rxjava3:rxjava:3.1.5")
-// https://mvnrepository.com/artifact/com.zaxxer/HikariCP
-//	implementation("com.zaxxer:HikariCP:5.0.1")
 
-//	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework:spring-jdbc")
-
-//	runtimeOnly("io.r2dbc:r2dbc-postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
